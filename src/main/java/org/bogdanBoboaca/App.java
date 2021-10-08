76bd52af224a8eb8f1d0053be4472fa351ae5108
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2021-10-08 21:19:24 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/bogdanBoboaca/App.java
@@ -0,0 +1,13 @@
+package org.bogdanBoboaca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
